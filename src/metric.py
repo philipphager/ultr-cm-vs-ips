40abfe1a86200ef4b1dce8213af9fc84df5f9a7a
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-07-26 16:35:37 +0200

    Update metric.py

--- src/metric.py
+++ src/metric.py
@@ -1,24 +1,15 @@
-import pandas as pd
 import torch
 
 from pytorchltr.evaluation import arp, ndcg
 
 
 def get_metrics(
-    y_predict: torch.Tensor,
-    y_true: torch.Tensor,
-    n: torch.Tensor,
-    q: torch.Tensor,
-    stage: str = "",
+    y_predict: torch.Tensor, y_true: torch.Tensor, n: torch.Tensor, prefix: str = ""
 ):
-    return pd.DataFrame(
-        {
-            "query": q,
-            "stage": stage,
-            "arp": arp(y_predict, y_true, n).detach().numpy(),
-            "ndcg@1": ndcg(y_predict, y_true, n, k=1).detach().numpy(),
-            "ndcg@5": ndcg(y_predict, y_true, n, k=5).detach().numpy(),
-            "ndcg@10": ndcg(y_predict, y_true, n, k=10).detach().numpy(),
-            "ndcg": ndcg(y_predict, y_true, n).mean().detach().numpy(),
-        }
-    )
+    return {
+        f"{prefix}arp": arp(y_predict, y_true, n).mean().detach(),
+        f"{prefix}ndcg@1": ndcg(y_predict, y_true, n, k=1).mean().detach(),
+        f"{prefix}ndcg@5": ndcg(y_predict, y_true, n, k=5).mean().detach(),
+        f"{prefix}ndcg@10": ndcg(y_predict, y_true, n, k=10).mean().detach(),
+        f"{prefix}ndcg": ndcg(y_predict, y_true, n).mean().detach(),
+    }
