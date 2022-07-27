686733bb5c8d28985886a8d07a534a5e56e71984
Philipp Hager <p.k.hager@uva.nl>
2022-07-27 07:49:28 +0200

    Add istella and mslr scritps

---
+++ scripts/mslr_dataset_size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python main.py -m \
+    hydra/launcher=submitit_slurm \
+    data=mslr30k \
+    experiment.name="dataset_size" \
+    "+experiment=ips_pointwise_unbiased,ips_pointwise_biased,cm_unbiased,cm_biased,cm_estimated" \
+    simulation.n_sessions=100,1_000,10_000,100_000,1_000_000,10_000_000,100_000_000 \
+    simulation.aggregate_clicks=True \
+    random_state=0,1,2,3,4,5,6,7,8,9
