e2af5b58c4d17a915dc90781a7ee9ed2358b0062
Philipp Hager <p.k.hager@uva.nl>
2022-07-28 19:47:26 +0200

    Evaluate momentum

--- scripts/hyperparameter.sh
+++ scripts/hyperparameter.sh
@@ -5,7 +5,7 @@ python main.py -m \
     data=yahoo \
     experiment.name="hyperparameters" \
     "+experiment=ips_pointwise_unbiased,cm_unbiased,cm_estimated" \
-    model.optimizer=sgd,adam,adagrad \
+    model.optimizer=sgd,sgd-momentum,adam,adagrad \
     model.learning_rate=0.1,0.05,0.01,0.005,0.001,0.0005,0.0001 \
     simulation.n_sessions=100_000_000 \
     simulation.aggregate_clicks=True \
