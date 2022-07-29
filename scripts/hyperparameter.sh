e86c031b6a2a70ec6f28f45c3a0c226573e65a69
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-07-29 10:05:59 +0200

    Update hyperparameter.sh

--- scripts/hyperparameter.sh
+++ scripts/hyperparameter.sh
@@ -2,10 +2,10 @@
 
 python main.py -m \
     hydra/launcher=submitit_slurm \
-    data=yahoo \
+    data=mslr30k \
     experiment.name="hyperparameters" \
     "+experiment=ips_pointwise_unbiased,cm_unbiased,cm_estimated" \
-    model.optimizer=sgd,sgd-momentum,adam,adagrad \
+    model.optimizer=sgd,adam,adagrad \
     model.learning_rate=0.1,0.05,0.01,0.005,0.001,0.0005,0.0001 \
     simulation.n_sessions=100_000_000 \
     simulation.aggregate_clicks=True \
