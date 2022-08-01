ab1c87fbc6e0d668e65cba3aec57970ff173c095
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-08-01 12:07:36 +0200

    Update lr

--- scripts/istella_dataset_size.sh
+++ scripts/istella_dataset_size.sh
@@ -4,7 +4,7 @@ python main.py -m \
     hydra/launcher=submitit_slurm \
     data=istella \
     experiment.name="dataset_size" \
-    "+experiment=cm_estimated" \
+    "+experiment=cm_unbiased" \
     simulation.n_sessions=100,1_000,10_000,100_000,1_000_000,10_000_000,100_000_000 \
     simulation.aggregate_clicks=True \
     random_state=0,1,2,3,4,5,6,7,8,9
