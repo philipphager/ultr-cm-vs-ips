dbea7971b30be05f1b678f9b4509b64c4319dbcf
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-08-02 13:11:02 +0200

    Create yahoo_position_bias_strength.sh

---
+++ scripts/yahoo_position_bias_strength.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python main.py -m \
+    hydra/launcher=submitit_slurm \
+    data=yahoo \
+    experiment.name="position_bias_strength" \
+    "+experiment=ips_pointwise_unbiased,cm_unbiased" \
+    simulation.n_sessions=100_000_000 \
+    simulation.aggregate_clicks=True \
+    simulation.simulator.user_model.position_bias=0,0.5,1.0,1.5,2.0 \
+    random_state=0
