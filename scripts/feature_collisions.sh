b6ff1bcb755821520286b0194992c0c4c5ad9123
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-08-03 13:42:13 +0200

    Update feature_collisions.sh

--- scripts/feature_collisions.sh
+++ scripts/feature_collisions.sh
@@ -5,7 +5,7 @@ python main.py -m \
     data=synthetic \
     experiment.name="feature_collisions" \
     "+experiment=ips_pointwise_unbiased,cm_unbiased,cm_estimated" \
-    data.perc_feature_collision=0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9
+    data.perc_feature_collision=0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9 \
     simulation.n_sessions=100_000_000 \
     simulation.aggregate_clicks=True \
     random_state=0,1,2,3,4,5,6,7,8,9
